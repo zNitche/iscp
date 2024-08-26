5bb7caee9f5f10864f551f080a00c7ce6cf300e8
zNitche <53448802+zNitche@users.noreply.github.com>
2024-08-26 21:43:32 +0200

    tweaked tokens_management.py parameters

--- tokens_management.py
+++ tokens_management.py
@@ -5,35 +5,34 @@ from iscep import auth_management
 
 def main(args):
     tokens_path: str = args.tokens_path
-    add: bool = args.add
-    remove: bool = args.remove
-    owner: str | None = args.owner
+    add_token_owner: str = args.add
+    remove_token_owner: str = args.remove
 
-    if add and owner:
-        auth_management.add_token(owner, tokens_path)
+    if add_token_owner:
+        auth_management.add_token(add_token_owner, tokens_path)
 
-    elif remove and owner:
-        auth_management.remove_token(owner, tokens_path)
+    elif remove_token_owner:
+        auth_management.remove_token(remove_token_owner, tokens_path)
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    tokens_default_path = os.path.join(os.getcwd(), "tokens.json")
 
-    parser.add_argument("--tokens_path",
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    tokens_default_path = os.path.join(current_dir, "tokens.json")
+
+    parser.add_argument("--tokens-path",
                         type=str,
                         default=tokens_default_path,
                         help="tokens json path",
-                        required=True)
+                        required=False)
 
-    parser.add_argument("--add", action=argparse.BooleanOptionalAction,
-                        type=bool, default=False, help="add auth token for owner", required=False)
+    parser.add_argument("--add",
+                        type=str, default=None, help="owner of auth token, to add", required=False)
 
-    parser.add_argument("--remove", action=argparse.BooleanOptionalAction,
-                        type=bool, default=False, help="remove auth token for owner", required=False)
+    parser.add_argument("--remove",
+                        type=str, default=None, help="owner of auth token, to remove", required=False)
 
-    parser.add_argument("--owner",
-                        type=str, default=None, help="owner of auth token, use with --add \ --remove", required=False)
     opt = parser.parse_args()
 
     return opt
